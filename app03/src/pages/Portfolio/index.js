548fcd51a5ff8f771f1ba6540a06eb07e6327e0b
Alex-Hugo <100318918+Alex-Hugo@users.noreply.github.com>
2022-11-19 16:57:58 -0300

    Add files via upload

---
+++ app03/src/pages/Portfolio/index.js
@@ -0,0 +1,13 @@
+import Header from "../../components/header";
+
+function Portfolio(){
+    return(
+        <div className="app">
+                <Header/>
+                <h1>Aqui é a página sobre os meus projetos!</h1>
+
+                <h2>Nesse 1 ano e meio que estudo Sistemas Web, já foram requisitados alguns projetos, alguns grandes e outros menores. A grande maioria deles envolvendo HTML, JS e CSS. Mais recentemente estamos estudando e desenvolvendo aplicações em ReactJS. Projetos de páginas pessoas, manipulações de objetos, pequenas integrações com uma base de dados (como o Back4App) entre outras coisas.</h2>
+        </div>
+    );
+
+}export default Portfolio
