8a89f31a0cadaa97acc500c32f7c625778c99205
Alex-Hugo <100318918+Alex-Hugo@users.noreply.github.com>
2022-11-19 16:57:17 -0300

    Add files via upload

---
+++ app03/src/pages/Hobbies/index.js
@@ -0,0 +1,13 @@
+import Header from "../../components/header";
+
+function Hobbies(){
+    return(
+        <div className="app">
+                <Header/>
+                <h1>Aqui é a página sobre os meus hobbies</h1>
+
+                <h2>Gosto muito de tocar violão, flauta, praticar esportes como futebol, basquete, natação. Também curto cozinhar, ver filmes e ler livros.</h2>
+        </div>
+    );
+
+}export default Hobbies
