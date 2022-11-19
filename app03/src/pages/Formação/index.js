05a12fc08ec3aeb60817b0b378d18f8ba0e46669
Alex-Hugo <100318918+Alex-Hugo@users.noreply.github.com>
2022-11-19 16:56:36 -0300

    Add files via upload

---
+++ app03/src/pages/Formação/index.js
@@ -0,0 +1,13 @@
+import Header from "../../components/header";
+
+function Formação(){
+    return(
+        <div className="app">
+                <Header/>
+                <h1>Aqui é a página sobre a minha formação!</h1>
+
+                <h2>Cursei o ensino fundamental e médio no Colégio de Aplicação da UFPE. Atualmente curso Engenharia de Materiais na Universidade Federal de Pernambuco, e a noite também assisto aulas de Sistemas para Internet na UNICAP.</h2>
+        </div>
+    );
+
+}export default Formação
