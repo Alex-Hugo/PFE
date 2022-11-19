38c52ca9dc83c5f6ac88681fcfc293c175c2b487
Alex-Hugo <100318918+Alex-Hugo@users.noreply.github.com>
2022-11-19 16:58:00 -0300

    Add files via upload

---
+++ app03/src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import Header from "../../components/header";
+
+
+
+function Home(){
+    return(
+      
+        <div className="app">
+                <Header/>
+                <h1>Aqui é a página principal!</h1>
+                <h2>Sou um recifense que gosta muito da minha terra, e aqui irei contar um pouco para você sobre mim!</h2>
+                </div>
+
+    );
+
+}export default Home
